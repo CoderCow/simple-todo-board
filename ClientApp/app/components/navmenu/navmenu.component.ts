7b9c897b063c8752cc6ec27423b9d7bdc30ea615
David Kay Posmyk <KayPosmyk@gmx.de>
2017-11-06 09:47:44 +0200

    feat (menu-bar): add simple material menu-bar

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -3,20 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "nav-menu",
   templateUrl: "./navmenu.component.html",
-  styleUrls: ["./navmenu.component.css"]
+  styleUrls: ["./navmenu.component.scss"]
 })
-export class NavMenuComponent {
-  public collapse = "collapse";
-
-  public collapseNavbar(): void {
-    if (this.collapse.length > 1) {
-      this.collapse = "";
-    } else {
-      this.collapse = "collapse";
-    }
-  }
-
-  public collapseMenu() {
-    this.collapse = "collapse";
-  }
-}
+export class NavMenuComponent {}
