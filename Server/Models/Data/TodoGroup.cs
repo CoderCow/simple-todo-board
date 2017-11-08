5d05b575adcb20afa8b96adc4c4d0fea402470ab
David Kay Posmyk <email>
2017-11-08 12:17:39 +0200

    add data models

---
+++ Server/Models/Data/TodoGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleTodoList.Server.Models.Data {
+  public class TodoGroup {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(30, MinimumLength = 1)]
+    public string Title { get; set; }
+
+    public ICollection<TodoItem> Todos { get; set; }
+  }
+}
---
+++ Server/Models/Data/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleTodoList.Server.Models.Data {
+  public class TodoItem {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public TodoGroup Group { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
+    public string Title { get; set; }
+
+    [DataType(DataType.MultilineText)]
+    [StringLength(1024)]
+    public string DescriptionHtml { get; set; }
+
+    [Required]
+    public DateTime TimeOfCreation { get; set; }
+
+    [Required]
+    public DateTime TimeLastEdited { get; set; }
+  }
+}
