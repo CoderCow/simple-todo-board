ed4ed54b6dd00f39294be9c5df7588cdcc3714a2
David Kay Posmyk <email>
2017-11-09 08:46:09 +0200

    add db seed data

--- Server/Data/DbInitializer.cs
+++ Server/Data/DbInitializer.cs
@@ -1,34 +1,59 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleTodoList;
 using SimpleTodoList.Models;
+using SimpleTodoList.Server.Models.Data;
 
 namespace SimpleTodoList.Data {
   public static class DbInitializer {
     public static void Initialize(SpaDbContext context) {
       context.Database.EnsureCreated();
 
-      if (context.User.Any())
-        return; // DB has been seeded
+      SeedTodoGroupsAndItems(context);
+    }
 
-      var users = new User[] {
-        new User() {Name = "Mark Pieszak"},
-        new User() {Name = "Abrar Jahin"},
-        new User() {Name = "hakonamatata"},
-        new User() {Name = "LiverpoolOwen"},
-        new User() {Name = "Ketrex"},
-        new User() {Name = "markwhitfeld"},
-        new User() {Name = "daveo1001"},
-        new User() {Name = "paonath"},
-        new User() {Name = "nalex095"},
-        new User() {Name = "ORuban"},
-        new User() {Name = "Gaulomatic"}
-      };
+    private static void SeedTodoGroupsAndItems(SpaDbContext context) {
+      if (context.TodoGroups.Any())
+        return;
 
-      foreach (var s in users)
-        context.User.Add(s);
+      context.TodoGroups.AddRange(
+        new TodoGroup {
+          Title = "Todo",
+          Todos = new List<TodoItem> {
+            new TodoItem {
+              Title = "Task 1",
+              DescriptionHtml =
+                "Lorem ipsumLorem ipsum dolor sit amet, consetetur sadipscing elitr.\nSed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+              UserOrder = 0,
+              TimeOfCreation = DateTime.UtcNow,
+              TimeLastEdited = DateTime.UtcNow
+            },
+            new TodoItem {
+              Title = "Task 2",
+              DescriptionHtml = "Lorem ipsumLorem ipsum dolor sit amet, consetetur sadipscing elitr.",
+              UserOrder = 1,
+              TimeOfCreation = DateTime.UtcNow - TimeSpan.FromDays(3),
+              TimeLastEdited = DateTime.UtcNow
+            }
+          }
+        },
+        new TodoGroup {
+          Title = "Doing",
+          Todos = new List<TodoItem> {
+            new TodoItem {
+              Title = "Task 3",
+              DescriptionHtml =
+                "Lorem ipsumLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.",
+              UserOrder = 0,
+              TimeOfCreation = DateTime.UtcNow - TimeSpan.FromDays(14),
+              TimeLastEdited = DateTime.UtcNow - TimeSpan.FromDays(8)
+            }
+          }
+        }
+      );
 
       context.SaveChanges();
     }
